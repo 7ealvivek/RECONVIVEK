cbc25c559452ecdf20a11c07065d74cba7eb386c
Vivek <vivekkumar_ch22b4_02@dtu.ac.in>
2023-07-21 19:22:08 +0530

    GG.py
    updated

--- vps_script.py
+++ vps_script.py
@@ -4,86 +4,90 @@ import sys
 import subprocess
 import os
 
-def get_subdomains(target):
-    subfinder_cmd = f"subfinder -d {target} -all -config subfinder_config.yaml -o subfinder_output.txt"
-    amass_cmd = f"amass enum -passive -norecursive -noalts -d {target} -config amass_config.ini -o amass_output.txt"
+def get_subdomains(target, output_dir):
+    if os.path.isfile(target):
+        # Use amass to enumerate subdomains from a file
+        amass_cmd = f"amass enum -passive -norecursive -noalts -df {target} -config amass_config.ini -o {output_dir}/amass_output.txt"
+    else:
+        # Use amass to enumerate subdomains from a single domain
+        amass_cmd = f"amass enum -passive -norecursive -noalts -d {target} -config amass_config.ini -o {output_dir}/amass_output.txt"
+    
+    subfinder_cmd = f"subfinder -d {target} -all -config subfinder_config.yaml -o {output_dir}/subfinder_output.txt"
     subprocess.run(subfinder_cmd, shell=True)
     subprocess.run(amass_cmd, shell=True)
 
-def combine_subdomains():
+def combine_subdomains(output_dir):
     all_subdomains = set()
-    with open("subfinder_output.txt") as f:
-        all_subdomains.update(f.read().splitlines())
-    with open("amass_output.txt") as f:
-        all_subdomains.update(f.read().splitlines())
-    return sorted(list(all_subdomains))
+    subfinder_output_file = f"{output_dir}/subfinder_output.txt"
+    amass_output_file = f"{output_dir}/amass_output.txt"
 
-def run_httpx_scan(subdomains):
-    httpx_cmd = f"httpx -l httpx_subdomains.txt -ports 80,443 -title -o httpx_output.txt"
-    with open("httpx_subdomains.txt", "w") as f:
+    with open(subfinder_output_file) as f:
+        all_subdomains.update(f.read().splitlines())
+    with open(amass_output_file) as f:
+        all_subdomains.update(f.read().splitlines())
+
+    sorted_subdomains = sorted(list(all_subdomains))
+    with open(f"{output_dir}/sorted_subdomains.txt", "w") as f:
+        f.write("\n".join(sorted_subdomains))
+
+    return sorted_subdomains
+
+def run_httpx_scan(subdomains, output_dir):
+    httpx_cmd = f"httpx -l {output_dir}/httpx_subdomains.txt -ports 80,443 -title -o {output_dir}/httpx_output.txt"
+    with open(f"{output_dir}/httpx_subdomains.txt", "w") as f:
         f.write("\n".join(subdomains))
     subprocess.run(httpx_cmd, shell=True)
 
-def run_rustscan(subdomains):
-    new_ports = set()
-    with open("sorted_subdomains.txt", "w") as f:
+def run_rustscan(subdomains, output_dir):
+    with open(f"{output_dir}/sorted_subdomains.txt", "w") as f:
         f.write("\n".join(subdomains))
 
-    rustscan_cmd = f"rustscan -a sorted_subdomains.txt -r 1000-65000 --ulimit 10000"
+    rustscan_cmd = f"rustscan -a {output_dir}/sorted_subdomains.txt -r 1000-65000 --ulimit 10000"
     result = subprocess.check_output(rustscan_cmd, shell=True, universal_newlines=True)
-    new_ports.update(set(map(int, result.strip().splitlines()[1:])))
+    new_ports = set(map(int, result.strip().splitlines()[1:]))
+
+    with open(f"{output_dir}/rustscan_output.txt", "w") as f:
+        f.write(result)
+
     return new_ports
 
-def notify_new_subdomain_and_ports(subdomains, new_ports):
-    if os.path.isfile("previous_subdomains.txt"):
-        with open("previous_subdomains.txt") as f:
-            previous_subdomains = set(f.read().splitlines())
-    else:
-        previous_subdomains = set()
-
-    new_subdomains = set(subdomains) - previous_subdomains
-
-    if new_subdomains:
-        for subdomain in new_subdomains:
-            # Use the 'notify' tool to send a separate notification for each new subdomain
-            notify_cmd = f"notify -title 'New Subdomain Discovered' -message '{subdomain}'"
-            os.system(notify_cmd)
-
-    if new_ports:
-        # Use the 'notify' tool to send a notification for new open ports
-        notify_cmd = f"notify -title 'New Open Ports Discovered' -message 'New open ports: {', '.join(map(str, new_ports))}'"
+def notify_desktop(title, message):
+    try:
+        notify_cmd = f"notify -title '{title}' -message '{message}'"
         os.system(notify_cmd)
-
-    with open("previous_subdomains.txt", "w") as f:
-        f.write("\n".join(subdomains))
+    except Exception as e:
+        print(f"Error while sending notification: {e}")
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 subdomain_port_scan.py <path_to_target_file>")
+        print("Usage: python3 subdomain_port_scan.py <path_to_target_or_target_file>")
         sys.exit(1)
 
-    target_file = sys.argv[1]
-    if not os.path.isfile(target_file):
-        print(f"Error: The target file '{target_file}' does not exist.")
-        sys.exit(1)
-
-    with open(target_file) as f:
-        target_domains = f.read().splitlines()
+    target_input = sys.argv[1]
+    if os.path.isfile(target_input):
+        with open(target_input) as f:
+            target_domains = f.read().splitlines()
+    else:
+        target_domains = [target_input]
 
     if not target_domains:
-        print("Error: The target file is empty.")
+        print("Error: The target input is empty.")
         sys.exit(1)
 
     for target in target_domains:
         print(f"Scanning target domain: {target}")
 
-        get_subdomains(target)
-        subdomains = combine_subdomains()
+        output_dir = f"output_{target.replace('.', '_')}"
+        os.makedirs(output_dir, exist_ok=True)
 
-        new_ports = run_rustscan(subdomains)
-        notify_new_subdomain_and_ports(subdomains, new_ports)
+        get_subdomains(target, output_dir)
+        subdomains = combine_subdomains(output_dir)
 
-        run_httpx_scan(subdomains)
+        new_ports = run_rustscan(subdomains, output_dir)
+        notify_desktop(f"New Subdomains and Ports Discovered for {target}",
+                       f"Subdomains: {', '.join(subdomains)}\nPorts: {', '.join(map(str, new_ports))}")
+
+        run_httpx_scan(subdomains, output_dir)
 
         print(f"Scan completed for target domain: {target}\n")
 
